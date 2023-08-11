9f54bb05dde75a945dd06c7bda26e7ef25dca860
Siddharth <siddharthpillai2001@gmail.com>
2023-08-11 10:54:46 +0530

    hands on 1

---
+++ Hands-on-1/1/1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+int main(){
+	int sl = symlink("tempfile","tempSL");
+	if(sl<0){
+		perror("Failed");
+		return 1;
+	}
+
+	int hl = symlink("tempfile","tempHL");
+	if(hl<0){
+		perror("Failed");
+		return 1;
+	}
+
+	int e = mknod("tempFIFO",S_IFIFO|0666,0);
+	if(e<0)	perror("Failed");
+	return 0;
+}
