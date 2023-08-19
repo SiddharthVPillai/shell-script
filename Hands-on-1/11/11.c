2c405aee8aff2ee558f9feee4e0bea48f85d88ee
Siddharth <siddharthpillai2001@gmail.com>
2023-08-19 11:44:56 +0530

    Program 11

---
+++ Hands-on-1/11/11.c
@@ -0,0 +1,27 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
+
+int main(){
+	int fd1 = open("file.txt",O_RDWR);
+	int fd2 = dup(fd1);
+	if(fd1==-1 || fd2==-1){
+		printf("Failed to open file\n");
+		return -1;
+	}	
+	write(fd1,"Written with fd\n",16);
+	write(fd2,"Written with dup() fd\n",22);
+	printf("fd1-%d, fd2-%d ",fd1,fd2);
+	
+	int fd3 = dup2(fd1,6); 
+	int fd4 = fcntl(fd1, F_DUPFD);
+	if(fd3==-1 || fd4==-1){
+		printf("Error creating copy\n");
+		return -1;
+	}
+	write(fd3,"Written with dup2() fd\n",23);
+	write(fd4,"Written with fcntl() fd\n",24);
+	printf("fd3-%d, fd4-%d ",fd3,fd4);
+	getchar();
+	return 0;
+}
